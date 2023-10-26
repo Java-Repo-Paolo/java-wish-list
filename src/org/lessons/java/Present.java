b22fa0dedd5931d912979449010f647c0af0e17a
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-26 14:40:28 +0200

    Commit 2 - mod Present Class

--- src/org/lessons/java/Present.java
+++ src/org/lessons/java/Present.java
@@ -1,21 +1,36 @@
 package org.lessons.java;
 
 public class Present {
+
+    //ATTRIBUTI
   private String subject;
   private String description;
 
-
+    //COSTRUTTORI
     public Present(String subject, String description) {
         this.subject = subject;
         this.description = description;
     }
 
+
+    //METODI
+
+    //GETTER
     public String getDescription() {
         return description;
     }
 
     public String getSubject() {
         return subject;
+    }
+
+    //SETTER
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
 
