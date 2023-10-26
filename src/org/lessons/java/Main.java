84740c163b6a9eb30561cfd19af28b3168419627
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-26 17:03:36 +0200

    Commit 4 - add write in file with scanner

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,34 +1,80 @@
 package org.lessons.java;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        readFile();
+        System.out.println("\n");
+
         Scanner scanner = new Scanner(System.in);
         List<Present> presents = new ArrayList<>();
 
         boolean exit = false;
 
-        while(!exit){
+        while (!exit) {
             System.out.println("Insert new person? yes/no");
             String choice = scanner.nextLine();
-            if(choice.equals("yes")){
+            if (choice.equals("yes")) {
                 System.out.print("Subject: ");
                 String subject = scanner.nextLine();
                 System.out.print("Description: ");
                 String description = scanner.nextLine();
                 presents.add(new Present(subject, description));
                 System.out.println("Your list contains: " + presents.size() + " present/s");
-            } else if (choice.equals("no")){
+            } else if (choice.equals("no")) {
                 exit = true;
-            }else {
+            } else {
                 System.out.println("invalid answer");
             }
         }
         System.out.println(presents);
+        System.out.println("Write in the file");
+        try {
+            FileWriter myWriter = new FileWriter("presentlist.txt", true);
+            for (Present present : presents) {
+                myWriter.write("\n" + "Present" + "\n");
+                myWriter.write("Subject: " + present.getSubject() + "\n");
+                myWriter.write("Description: " + present.getDescription() + "\n");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("invalid file");
+        }
+
         scanner.close();
+    }
+
+
+
+    // metodo che legge il file
+    private static void readFile(){
+        File file = new File("presentlist.txt");
+        if (file.exists()) {
+            System.out.println("Presents list: ");
+            try (Scanner fileReader = new Scanner(file)) {
+                while (fileReader.hasNextLine()) {
+
+                    String line = fileReader.nextLine();
+                    System.out.println(line);
+                }
+
+            } catch(FileNotFoundException e){
+                System.out.println("Unable to read file");
+            }
+            System.out.println("\n ");
+        }
+        else {
+            System.out.println("There is not a file yet");
+        }
     }
 }
